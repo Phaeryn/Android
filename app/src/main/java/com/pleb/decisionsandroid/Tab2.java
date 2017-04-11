53808bf69d21cb7f7007cd9a1990c9400df525e5
Alternatif <maxime.mtn@gmail.com>
2017-04-11 22:24:23 +0800

    first commit Tabbed interface, each tab has 1 layout and 1 class

---
+++ app/src/main/java/com/pleb/decisionsandroid/Tab2.java
@@ -0,0 +1,22 @@
+package com.pleb.decisionsandroid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Maxime on 11/04/2017.
+ */
+
+public class Tab2 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/pleb/decisionsandroid/Tab3.java
@@ -0,0 +1,22 @@
+package com.pleb.decisionsandroid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Maxime on 11/04/2017.
+ */
+
+public class Tab3 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3, container, false);
+        return rootView;
+    }
+}
